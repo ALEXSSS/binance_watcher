c0a31ea97ac6f1ca010a6d9e63f1600b8a475994
ALEXSSS <ssstik4@gmail.com>
2025-02-25 20:30:02 +0300

    fix stupid mistake with additional wrapping

--- src/order_book.rs
+++ src/order_book.rs
@@ -1,5 +1,4 @@
 use crate::messages::{BookDepthUpdate, FullBook, LevelApi};
-use std::cell::{Cell, RefCell};
 use std::fmt::{Display, Formatter};
 
 /// OrderBook maintained during application runtime.
@@ -7,12 +6,12 @@ use std::fmt::{Display, Formatter};
 /// This struct is Send, so it safe to use it cross-await call as we do (not simultaneously)
 #[derive(Default)]
 pub struct OrderBook {
-    last_update_id: Cell<u64>,
-    levels: Cell<u32>,
+    last_update_id: u64,
+    levels: u32,
     symbol: String,
-    bid: RefCell<Vec<Level>>,
-    ask: RefCell<Vec<Level>>,
-    is_just_initialised: Cell<bool>,
+    bid: Vec<Level>,
+    ask: Vec<Level>,
+    is_just_initialised: bool,
 }
 
 /// My thoughts:
@@ -27,7 +26,7 @@ pub struct Level {
 impl OrderBook {
     pub fn new(levels: u32, symbol: String) -> Self {
         Self {
-            levels: Cell::new(levels),
+            levels,
             symbol: symbol.clone(),
             ..Default::default()
         }
@@ -62,8 +61,7 @@ impl OrderBook {
     }
 
     pub fn get_best_bid(&self) -> Result<Level, String> {
-        let reference = &self.bid.borrow();
-        let level_option: Option<&Level> = reference.get(0);
+        let level_option: Option<&Level> = self.bid.get(0);
         match level_option {
             None => Err("empty bid".to_string()),
             Some(level) => Ok(level.clone()),
@@ -71,8 +69,7 @@ impl OrderBook {
     }
 
     pub fn get_best_ask(&self) -> Result<Level, String> {
-        let reference = &self.ask.borrow();
-        let level_option: Option<&Level> = reference.get(0);
+        let level_option: Option<&Level> = self.ask.get(0);
         match level_option {
             None => Err("empty ask".to_string()),
             Some(level) => Ok(level.clone()),
@@ -80,19 +77,19 @@ impl OrderBook {
     }
 
     pub fn apply_full_book_from_http_api(&mut self, book: &FullBook) {
-        self.last_update_id.set(book.last_update_id);
-        self.is_just_initialised.set(true);
+        self.last_update_id = book.last_update_id;
+        self.is_just_initialised = true;
 
         // bid
-        self.bid.get_mut().clear();
+        self.bid.clear();
         for level in &book.bids {
-            self.bid.get_mut().push(level_api_to_level(&level));
+            self.bid.push(level_api_to_level(&level));
         }
 
         // ask
-        self.ask.get_mut().clear();
+        self.ask.clear();
         for level in &book.asks {
-            self.ask.get_mut().push(level_api_to_level(&level));
+            self.ask.push(level_api_to_level(&level));
         }
 
         self.trim()
@@ -109,7 +106,7 @@ impl OrderBook {
             return false;
         }
         // check that previous final id was last_id
-        if !self.is_just_initialised.get() && self.last_update_id.get() != book.pu {
+        if !self.is_just_initialised && self.last_update_id != book.pu {
             return false;
         }
 
@@ -120,7 +117,7 @@ impl OrderBook {
         for level in &book.a {
             self.apply_ask(&level);
         }
-        self.last_update_id.set(book.u);
+        self.last_update_id = book.u;
         self.trim();
 
         true
@@ -128,11 +125,11 @@ impl OrderBook {
 
     // utils
     fn is_update_applied(&self, book_update: &BookDepthUpdate) -> bool {
-        self.last_update_id.get() > book_update.u
+        self.last_update_id > book_update.u
     }
 
     fn is_eligible_for_update(&self, book_update: &BookDepthUpdate) -> bool {
-        let last_update_id = self.last_update_id.get();
+        let last_update_id = self.last_update_id;
         book_update.U <= last_update_id && last_update_id <= book_update.u
     }
 
@@ -143,12 +140,11 @@ impl OrderBook {
         Self::do_apply_to_level(&mut self.ask, api_level, true)
     }
 
-    fn do_apply_to_level(levels: &mut RefCell<Vec<Level>>, api_level: &LevelApi, ascending: bool) {
+    fn do_apply_to_level(levels: &mut Vec<Level>, api_level: &LevelApi, ascending: bool) {
         let level_update = level_api_to_level(api_level);
-        let result = Self::look_for_level(level_update.price, levels.borrow().as_ref(), ascending);
+        let result = Self::look_for_level(level_update.price, levels, ascending);
         match result {
             Ok(index) => {
-                let levels = levels.get_mut();
                 if Self::floats_equal(level_update.price, 0.0) {
                     // TBD: could be done much more efficiently
                     levels.remove(index);
@@ -160,7 +156,6 @@ impl OrderBook {
                 }
             }
             Err(index) => {
-                let levels = levels.get_mut();
                 if Self::floats_equal(level_update.price, 0.0) {
                     // ignore
                 } else {
@@ -192,8 +187,8 @@ impl OrderBook {
     }
 
     fn trim(&mut self) {
-        self.bid.get_mut().truncate(self.levels.get() as usize);
-        self.ask.get_mut().truncate(self.levels.get() as usize)
+        self.bid.truncate(self.levels as usize);
+        self.ask.truncate(self.levels as usize)
     }
 
     fn write_level(
@@ -238,10 +233,10 @@ impl Display for OrderBook {
         writeln!(f, "|         bid         |     |         ask         |")?;
         writeln!(f, "|   qty    |   price  |     |   qty    |  price   |")?;
         writeln!(f, "---------------------------------------------------")?;
-        for index in 0..self.levels.get() as usize {
-            let reference = &self.bid.borrow();
+        for index in 0..self.levels as usize {
+            let reference = &self.bid;
             let bid_level: Option<&Level> = reference.get(index);
-            let reference = &self.ask.borrow();
+            let reference = &self.ask;
             let ask_level: Option<&Level> = reference.get(index);
             self.write_level(f, bid_level, ask_level);
         }
@@ -264,36 +259,36 @@ mod test {
     fn get_best_bid_test() {
         let mut book = OrderBook::default();
 
-        book.bid.get_mut().push(Level {
+        book.bid.push(Level {
             quantity: 1.0,
             price: 20.0,
         });
-        book.bid.get_mut().push(Level {
+        book.bid.push(Level {
             quantity: 1.0,
             price: 19.0,
         });
-        book.bid.get_mut().push(Level {
+        book.bid.push(Level {
             quantity: 1.0,
             price: 18.0,
         });
-        book.bid.get_mut().push(Level {
+        book.bid.push(Level {
             quantity: 1.0,
             price: 17.0,
         });
 
-        book.ask.get_mut().push(Level {
+        book.ask.push(Level {
             quantity: 1.0,
             price: 21.0,
         });
-        book.ask.get_mut().push(Level {
+        book.ask.push(Level {
             quantity: 1.0,
             price: 22.0,
         });
-        book.ask.get_mut().push(Level {
+        book.ask.push(Level {
             quantity: 1.0,
             price: 23.0,
         });
-        book.ask.get_mut().push(Level {
+        book.ask.push(Level {
             quantity: 1.0,
             price: 24.0,
         });
@@ -317,7 +312,7 @@ mod test {
     #[test]
     fn apply_http_full_book_apply_test() {
         let mut book = OrderBook::default();
-        book.levels.set(3);
+        book.levels = 3;
 
         let http_book = FullBook {
             last_update_id: 100500,
@@ -367,12 +362,12 @@ mod test {
                 price: 6.0
             }
         );
-        assert_eq!(book.bid.borrow().len(), 3);
-        assert_eq!(book.ask.borrow().len(), 3);
+        assert_eq!(book.bid.len(), 3);
+        assert_eq!(book.ask.len(), 3);
 
         // change levels param
 
-        book.levels.set(2);
+        book.levels = 2;
         book.apply_full_book_from_http_api(&http_book);
 
         assert_eq!(
@@ -389,15 +384,15 @@ mod test {
                 price: 6.0
             }
         );
-        assert_eq!(book.bid.borrow().len(), 2);
-        assert_eq!(book.ask.borrow().len(), 2);
+        assert_eq!(book.bid.len(), 2);
+        assert_eq!(book.ask.len(), 2);
     }
 
     #[test]
     fn apply_websocket_update_book_apply_test() {
         let mut book = OrderBook::default();
-        book.levels.set(3);
-        book.is_just_initialised.set(true);
+        book.levels = 3;
+        book.is_just_initialised = true;
 
         let ws_book = BookDepthUpdate {
             e: "".to_string(),
@@ -443,25 +438,25 @@ mod test {
         assert_eq!(succ, false);
 
         // 2) if book already applied update, then nothing should be done
-        book.last_update_id.set(100501);
+        book.last_update_id = 100501;
 
         let succ = book.apply_depth_book_update_from_websocket(&ws_book);
 
         assert_eq!(succ, true);
-        assert_eq!(book.bid.borrow().len(), 0);
-        assert_eq!(book.ask.borrow().len(), 0);
-        assert_eq!(book.last_update_id.get(), 100501);
+        assert_eq!(book.bid.len(), 0);
+        assert_eq!(book.ask.len(), 0);
+        assert_eq!(book.last_update_id, 100501);
 
         // 3) we are in range for update between U <= last_update_id <= u
 
-        book.last_update_id.set(100499);
+        book.last_update_id = 100499;
 
         let succ = book.apply_depth_book_update_from_websocket(&ws_book);
 
         assert_eq!(succ, true);
-        assert_eq!(book.bid.borrow().len(), 3);
-        assert_eq!(book.ask.borrow().len(), 3);
-        assert_eq!(book.last_update_id.get(), 100500);
+        assert_eq!(book.bid.len(), 3);
+        assert_eq!(book.ask.len(), 3);
+        assert_eq!(book.last_update_id, 100500);
 
         assert_eq!(
             book.get_best_bid().unwrap(),
